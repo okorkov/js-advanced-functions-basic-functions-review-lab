83b4998416a66a200c648045988e2f07bcadb371
okorkov <alex.okorkov@gmail.com>
2021-02-18 12:49:54 -0800

    commit tree 8ac82cf53c7fcf63dce412109ba8b6b18b2ef991

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(p="*") {
+  return function(param="special") {
+    return `You are ${p}${param}${p}!`
+  }
+}
+
+
+const Calculator = {
+  'add': function() {return 1 + 3;},
+  'subtract': function() {return 1 - 3;},
+  'multiply': function() {return 1 * 3;},
+  'divide': function() {return 10 / 5;}
+};
+
+function actionApplyer(start, arrayOfFunctions) {
+  if(arrayOfFunctions.length === 0) {return start;}
+    for(const fn of arrayOfFunctions) {
+      start = fn(start);
+    }
+  return start;
+}
